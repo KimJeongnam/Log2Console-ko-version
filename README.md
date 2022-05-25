4eb495698be1029becd64cc5784413d48ee0b1cc
jeongnam <kim910712@gmail.com>
2022-05-25 10:39:16 +0900

    src/Log2Console/Logs/LogMessage.cs  LIne:159 한글 인코딩, 개행(\r\n) 부분 수정

--- README.md
+++ README.md
@@ -1,8 +1,30 @@
-# Log2Console-write-ko
+# Log2Console
 
+## Project Description
+Log2Console is a development tool offering a nice UI to display, filter and search log messages from different logging services: Log4Net, Log4j and NLog. It can directly receive log events locally or remotely, or read them from a log file... It is written with C#.
 
-기존 Log2Console에서 Message Detail 항목 에서 한글이 깨지던 문제를 해결했습니다.
+## Main Features
+* View Remote Log Events: use a concept similar to Chainsaw Receivers.
+* Fully Customizable and Persistent Preferences (UI and Settings) per Windows Profile.
+* Dynamic Filtering, Grouping, Cycling and Searching.
+* Multiple Receiver suppport (since v1.1).
+* Windows 7 support: Taskbar icons (pause, disable autoscroll, clear all) and Taskbar progress (when messages are received).
+* Requires .NET Framework 2.0 only.
 
+## Receivers
+* File (Standard log or using log4j xml format)
+* UDP IP v4 and v6 (Provides compatibility with log4j and [NLog](docs/NLog.md))
+* .NET Remoting (Prefered receiver for log4net)
+* WinDebug (Receiver for OutputDebugString(), in C++)
+* MSMQ (Microsoft Message Queuing)
+* EventLog (Windows Event Log)
+* TCP IP v4 and v6 (primarily NLog for Silverlight)
+* Silverlight Socket Policy (clientaccesspolicy.xml)
 
+## Screenshots
+_**More [Screenshots](docs/Screenshots.md) here**_
 
-출처 : https://github.com/Statyk7/log2console
+![](docs/Home_Log2Console_3.png)
+
+## Download
+[Latest Release] https://github.com/Statyk7/log2console/releases
--- src/Log2Console/Utils.cs
+++ src/Log2Console/Utils.cs
@@ -7,40 +7,39 @@ using System.Windows.Forms;
 
 namespace Log2Console
 {
-  public static class utils
-  {
-    public static void Export2Excel(ListView listView, string FileName)
+    public static class utils
     {
-      using (StreamWriter sw = new StreamWriter(FileName, false))
-      {
-        sw.AutoFlush = true;
-        StringBuilder sb = new StringBuilder();
-        foreach (ColumnHeader ch in listView.Columns)
+        public static void Export2Excel(ListView listView, string FileName)
         {
-          sb.Append(ch.Text + ",");
+
+                using (StreamWriter sw = new StreamWriter(FileName, false))
+                {
+                    sw.AutoFlush = true;
+                    StringBuilder sb = new StringBuilder();
+                    foreach (ColumnHeader ch in listView.Columns)
+                    {
+                        sb.Append(ch.Text + ",");
+                    }
+                    sb.AppendLine();
+                    foreach (ListViewItem lvi in listView.Items)
+                    {
+                        foreach (ListViewItem.ListViewSubItem lvs in lvi.SubItems)
+                        {
+                            if (lvs.Text.Trim() == string.Empty)
+                                sb.Append(" ,");
+                            else
+                                sb.Append(lvs.Text + ",");
+                        }
+                        sb.AppendLine();
+                    }
+                    
+                    sw.Write(sb.ToString());
+                    sw.Close();
+                }
+                FileInfo fil = new FileInfo(FileName);
+                if (fil.Exists == true)
+                    MessageBox.Show("Process Completed", "Export to Excel", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
         }
-
-        sb.AppendLine();
-        foreach (ListViewItem lvi in listView.Items)
-        {
-          foreach (ListViewItem.ListViewSubItem lvs in lvi.SubItems)
-          {
-            if (lvs.Text.Trim() == string.Empty)
-              sb.Append(" ,");
-            else
-              sb.Append(lvs.Text + ",");
-          }
-
-          sb.AppendLine();
-        }
-
-        sw.Write(sb.ToString());
-        sw.Close();
-      }
-
-      FileInfo fil = new FileInfo(FileName);
-      if (fil.Exists == true)
-        MessageBox.Show("Process Completed", "Export to Excel", MessageBoxButtons.OK, MessageBoxIcon.Information);
     }
-  }
 }
